7b1e7913a6f7036c73332424dcce8374ebb4d4d0
duggurd <alexanderhaugerud@gmail.com>
2022-12-30 18:23:53 +0100

    replaced join() with +

--- NabolagAPI.py
+++ NabolagAPI.py
@@ -4,13 +4,13 @@ import json
 
 URL = "https://profil.nabolag.no/"
 
-def get_nabolag(finnkode: int) -> dict:
+def get_nabolag(finnkode: int|str) -> dict:
     """ Gets data about neighborhood near finnkode's location from "profil.nabolag.no"
 
     params:
     `finnkode` - valid finnkode
     """
-    res = requests.get("".join((URL, str(finnkode))))
+    res = requests.get(URL + str(finnkode))
 
     if res.status_code == 200:
         soup = bs4.BeautifulSoup(res.content, "html.parser")
